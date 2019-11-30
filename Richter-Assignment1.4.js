edc60e996a70cb5342857c4308c6a5974df24e9d
Emily Richter <emrichter1996@gmail.com>
2019-11-29 22:01:46 -0600

    Adding Richter-Assignment1.4.js

---
+++ Richter-Assignment1.4.js
@@ -0,0 +1,21 @@
+// Variable declaration
+var firstName;
+// Variable assignment
+firstName = "Emily";
+var lastName;
+lastName = "Richter";
+var age;
+age = "23";
+var street;
+street = "42 Wallaby Way";
+var city;
+city = "Sydney";
+var zipCode;
+zipCode = "34567";
+// Output
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
