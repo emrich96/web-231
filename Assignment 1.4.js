963424c0c272a0ecd9af9b92def2e88cbb62172c
Emily Richter <emrichter1996@gmail.com>
2019-11-29 21:51:20 -0600

    Adding Assignment 1.4.js

---
+++ Assignment 1.4.js
@@ -0,0 +1,18 @@
+var firstName;
+firstName = "Emily";
+var lastName;
+lastName = "Richter";
+var age;
+age = "23";
+var street;
+street = "42 Wallaby Way";
+var city;
+city = "Sydney";
+var zipCode;
+zipCode = "34567";
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
