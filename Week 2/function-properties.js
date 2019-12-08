12f784db084100c4fb98e8bbf93223f286d7b9de
Emily Richter <emrichter1996@gmail.com>
2019-12-08 03:16:13 -0600

    Adding Excercise 2.3 files

---
+++ Week 2/function-properties.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title:  function-properties.js
+; Author: Emily Richter
+; Date:   8 December 2019
+; Description: Demonstrates how to define and call function properties
+;===========================================
+*/
+// Header
+const header = require('./RichterEmily');
+console.log(header.display("Emily", "Richter", "Exercise 2.2"))
+
+// Function property definition
+myName.emily = "Emily"
+
+// Function, 0 parameters
+function myName() {
+  return myName.emily
+}
+
+// Output
+console.log('My name is ' +  myName())
