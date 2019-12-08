8ea933274d86895ecf56b3ae68c3f7ff2b7853e8
Emily Richter <emrichter1996@gmail.com>
2019-12-08 00:17:00 -0600

    Adding files for Excercise 2.2; created Week 2 folder

---
+++ Week 2/RichterEmily-header-test.js
@@ -0,0 +1,15 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso
+; Date:   8 December 2019
+; Modified By: Emily Richter
+; Description: Displays a formatted header
+;===========================================
+*/
+
+// Add an import statement
+const header = require('./RichterEmily');
+
+// Output
+console.log(header.display("Emily", "Richter", "Exercise 2.2"))
