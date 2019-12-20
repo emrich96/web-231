d88388bf51bc4249b41903c32ebb1c7f02696970
Emily Richter <emrichter1996@gmail.com>
2019-12-19 21:52:09 -0600

    Add Discussion 4.1

---
+++ Week 4/Richter-Discussion4.1.js
@@ -0,0 +1,32 @@
+/*
+============================================
+; Title:  Discussion 4.1
+; Author: Emily Richter
+; Date:   19 December 2019
+; Description: Create a program using an
+; array with at least 2 errors.
+;===========================================
+*/
+
+// Required import statement for header
+const header = require('../RichterEmily');
+// Output header
+console.log(header.display("Emily", "Richter", "Discussion 3.1"));
+
+// Declare array Serenity
+var serenity = ['Mal', 'Wash', 'Jayne', 'Zoe', 'Inara' 'Bester'];
+
+// Delete the fifth element, add four elements
+serenity.splice(5, 1, 'Kaylee', 'Book', 'Simon', 'River')
+
+// Define crew function
+function crew(serenity) {
+  for (i = 0; i < serenity.length; i++) {
+    console.log(serenity[])
+  }
+}
+
+// Call up crew function
+crew(serenity)
+
+//End program
