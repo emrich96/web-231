24cfb31bcf8b60974ab8717937031c593f293c5d
Emily Richter <emrichter1996@gmail.com>
2020-01-19 23:00:14 -0600

    Modify 6.4

--- Week 6/Richter-Assignment6.4.js
+++ Week 6/Richter-Assignment6.4.js
@@ -2,8 +2,8 @@
 =============================================
 ; Title:  Assignment 6.4
 ; Author: Emily Richter
-; Date:   14 January 2020
-; Description:
+; Date:   19 January 2020
+; Description: Nested object literal examples.
 ;============================================
 */
 
@@ -13,3 +13,31 @@
 const header = require('../RichterEmily');
 // Output header
 console.log(header.display("Emily", "Richter", "Assignment 5.4"));
+
+/*
+  Expected output:
+
+  FirstName LastName
+  Assignment 6.4
+  Today's Date
+  Ticket 2319 was created on <today' date> and assigned to employee Emily Richter (Programmer I).
+*/
+
+// Create object literal 'ticket' with nested object literal 'person'.
+var ticket = {
+  id: 2319,
+  name: "Account Support",
+  dateCreated: new Date().toLocaleDateString('en-US'),
+  priority: "low",
+  person: {
+    id: 113,
+    firstName: "Emily",
+    lastName: "Richter",
+    jobTitle: "Programmer I"
+  }
+};
+
+// Output
+console.log(`Ticket ${ticket.id} was created on ${ticket.dateCreated} and assigned to employee ${ticket.person.firstName} ${ticket.person.lastName} (${ticket.person.jobTitle}).`);
+
+// End program
